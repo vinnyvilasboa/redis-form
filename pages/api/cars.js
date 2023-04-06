25db5adfef07f9ec8a44bfe4e09e4c11aeb87213
vinnyvilasboa <vinnycesca@gmail.com>
2023-04-06 14:42:59 -0400

    car form + api

--- pages/api/cars.js
+++ pages/api/cars.js
@@ -1,5 +1,10 @@
 import { createCar } from "../../lib/redis";
 
+//the request creates a car id. The response will be 
 export default async function handler(req, res){
-    
+    //on call it will run createCar function and request the body
+    //req.body will be a JavaScript object containing the parsed JSON data
+    const id = await createCar(req.body);
+
+    res.status(200).json({id})
 }
