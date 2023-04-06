abe632c4cdc583248df7f90dab7f06532df990e3
vinnyvilasboa <vinnycesca@gmail.com>
2023-04-06 18:32:23 -0400

    up to date - bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import CarForm from "../lib/CarForm";
+import CarForm from '../lib/CarForm';
+import SearchForm from '../lib/SearchForm';
 
-export default function Home(){
-    return(
-        <div>
-            <h1>Create Car Form</h1>
-            <CarForm/>
-        </div>
-    )
+export default function Home(props) {
+  return (
+    <main>
+      <h1>Create a Car</h1>
+      <CarForm />
+      <hr />
+      <h1>Find a Car</h1>
+      <SearchForm />
+    </main>
+  );
 }
